f4a9df0c7d6946c339bcc78885d152ddde80c418
SHCHwei <wiwi328122@gmail.com>
2026-08-20 11:14:34 +0800

    add: GCP basic

--- main.go
+++ main.go
@@ -1,36 +1,50 @@
-
-// Sample run-helloworld is a minimal Cloud Run service.
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
+	firebase "firebase.google.com/go/v4"
 )
 
 func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
+	ctx := context.Background()
+	app, err := firebase.NewApp(ctx, nil) // 自動讀取 GCP 環境憑證
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
 
-	// Determine port for HTTP service.
+	authClient, err := app.Auth(ctx)
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+
+	http.HandleFunc("/api/protected", func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			http.Error(w, "Unauthorized: Missing Token", http.StatusUnauthorized)
+			return
+		}
+
+		idToken := strings.TrimPrefix(authHeader, "Bearer ")
+		token, err := authClient.VerifyIDToken(ctx, idToken)
+		if err != nil {
+			http.Error(w, "Unauthorized: Invalid Token", http.StatusUnauthorized)
+			return
+		}
+
+		// 驗證成功，取得 User UID
+		fmt.Fprintf(w, "Hello User UID: %s", token.UID)
+	})
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
-		log.Printf("defaulting to port %s", port)
 	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "World"
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
+	log.Printf("Listening on port %s", port)
+	http.ListenAndServe(":"+port, nil)
 }
